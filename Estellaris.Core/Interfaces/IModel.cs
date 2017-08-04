d791ca6ebf431da68e8dfe801d86ffe70e1dae35
Edio de Oliveira Zalewski <edio.o.zalewski@gmail.com>
2017-08-04 10:42:44 -0300

    deleted useless imodel

--- Estellaris.Core/Interfaces/IModel.cs
+++
@@ -1,8 +0,0 @@
-using System;
-
-namespace Estellaris.Core.Interfaces {
-  public interface IModel {
-    int Id { get; set; }
-    DateTime CreatedAt { get; set; }
-  }
-}
--- Estellaris.EF/Interfaces/IMapping.cs
+++ Estellaris.EF/Interfaces/IMapping.cs
@@ -1,9 +1,8 @@
-using Estellaris.Core.Interfaces;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace Estellaris.EF.Interfaces {
-  public interface IMapping<T> where T : class, IModel {
+  public interface IMapping<T> where T : class {
     void Map(EntityTypeBuilder<T> model);
   }
 }
