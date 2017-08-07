e6a479a0340d359a53ed3fda5b7e189a619034e3
Edio de Oliveira Zalewski <edio.o.zalewski@gmail.com>
2017-08-07 14:57:19 -0300

    add configs mapping and tests and fix some issues

--- Estellaris.Core/Configuration/Configs.cs
+++ Estellaris.Core/Configuration/Configs.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Configuration.Memory;
 
@@ -9,8 +10,8 @@ namespace Estellaris.Core {
   public class Configs {
     static bool _isLoaded { get; set; }
     public static IConfigurationRoot Configuration { get; private set; }
-    public static string Environment { get; protected set; } = "dev";
-    public static string BasePath { get; protected set; } = Directory.GetCurrentDirectory();
+    public static string Environment { get; protected set; }
+    public static string BasePath { get; protected set; }
 
     public static void Init(string environment = null, string basePath = null, IDictionary<string, string> inMemoryConfigs = null) {
       var builder = new ConfigurationBuilder();
@@ -22,11 +23,8 @@ namespace Estellaris.Core {
         }
       }
 
-      if (!string.IsNullOrWhiteSpace(environment))
-        Environment = environment;
-
-      if (!string.IsNullOrWhiteSpace(basePath))
-        BasePath = basePath;
+      Environment = !string.IsNullOrWhiteSpace(environment) ? environment : "dev";
+      BasePath = !string.IsNullOrWhiteSpace(basePath) ? basePath : Directory.GetCurrentDirectory();
 
       builder.SetBasePath(BasePath);
       builder.AddInMemoryCollection(memoryProvider);
@@ -44,12 +42,13 @@ namespace Estellaris.Core {
     public static T GetValueOrDefault<T>(string key, T _default) {
       CheckConfiguration();
       var value = GetValue<T>(key);
-      return value != null ? value : _default;
+      return EqualityComparer<T>.Default.Equals(value, default(T)) ? _default : value;
     }
 
     public static IEnumerable<T> GetArray<T>(string key) {
       CheckConfiguration();
-      return Configuration.GetSection(key).GetChildren().Select(_ => _.Value).OfType<T>();
+      var section = Configuration.GetSection(key);
+      return GetArrayOfElements<T>(section);
     }
 
     public static string GetConnectionString(string name = "Default") {
@@ -62,8 +61,69 @@ namespace Estellaris.Core {
       if (string.IsNullOrWhiteSpace(basePath))
         basePath = Directory.GetCurrentDirectory();
 
-      var localPath = Path.Combine(basePath, Configuration[key]);
+      var dirname = Configuration[key];
+
+      if (string.IsNullOrWhiteSpace(dirname))
+        return null;
+
+      var localPath = Path.Combine(basePath, dirname);
       return !Directory.Exists(localPath) ? Directory.CreateDirectory(localPath) : new DirectoryInfo(localPath);
+    }
+
+    public static T BindTo<T>(string key) {
+      CheckConfiguration();
+      var model = Activator.CreateInstance<T>();
+      var properties = typeof (T).GetProperties().ToList();
+      var section = Configuration.GetSection(key);
+      FillUpProperties(model, properties, section);
+      return model;
+    }
+
+    static IEnumerable<T> GetArrayOfElements<T>(IConfigurationSection section) {
+      CheckConfiguration();
+      if (section == null)
+        return Enumerable.Empty<T>();
+      return section
+        .GetChildren()
+        .Select(_ =>(T) Convert.ChangeType(_.Value, typeof (T)))
+        .ToList();
+    }
+
+    static object GetGenericArrayOfElements(IConfiguration section, Type type) {
+      var method = typeof (Configs).GetMethod("GetArrayOfElements", BindingFlags.Static | BindingFlags.NonPublic).MakeGenericMethod(type);
+      return method.Invoke(null, new [] { section });
+    }
+
+    static void FillUpProperties(object model, IEnumerable<PropertyInfo> properties, IConfigurationSection section) {
+      foreach(var property in properties) {
+        var value = section.GetSection(property.Name).Value;
+        if (!property.PropertyType.FullName.StartsWith("System.")) {
+          var subSection = section.GetSection(property.Name);
+          var subProperties = property.PropertyType.GetProperties().ToList();
+          var subModel = Activator.CreateInstance(property.PropertyType);
+          property.SetValue(model, subModel);
+          FillUpProperties(subModel, subProperties, subSection);
+        } else if (property.PropertyType.FullName.EndsWith("[]")) {
+          var type = Type.GetType(property.PropertyType.FullName.Replace("[]", ""));
+          var array = GetGenericArrayOfElements(section.GetSection(property.Name), type);
+          var toArray = array.GetType().GetMethod("ToArray");
+          property.SetValue(model, toArray.Invoke(array, null));
+        } else if (property.PropertyType.FullName.Contains(".Generic")) {
+          var type = property.PropertyType.GenericTypeArguments.First();
+          var array = GetGenericArrayOfElements(section.GetSection(property.Name), type);
+          property.SetValue(model, array);
+        } else {
+          object convertedValue = null;
+          if (!string.IsNullOrWhiteSpace(value))
+            convertedValue = Convert.ChangeType(value, property.PropertyType);
+          property.SetValue(model, convertedValue);
+        }
+      }
+    }
+
+    public static void Dispose() {
+      _isLoaded = false;
+      Configuration = null;
     }
 
     static void CheckConfiguration() {
---
+++ Estellaris.Tests/ConfigsTests.cs
@@ -0,0 +1,194 @@
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading;
+using Estellaris.Core;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Estellaris.Tests {
+  [TestClass]
+  public class ConfigsTests {
+     [TestMethod]
+    public void TestEnvironment() {
+      Configs.Init(environment: "invalid");
+      Assert.IsNull(Configs.GetValue<string>("EnvName"));
+      Configs.Dispose();
+
+      Configs.Init(environment: "dev");
+      Assert.AreEqual("Dev", Configs.GetValue<string>("EnvName"));
+      Configs.Dispose();
+
+      Configs.Init(environment: "hml");
+      Assert.AreEqual("Hml", Configs.GetValue<string>("EnvName"));
+      Configs.Dispose();
+    }
+
+    [TestMethod]
+    public void TestBasePath() {
+      var executingPath = Directory.GetCurrentDirectory();
+      Configs.Init();
+      Assert.AreEqual(executingPath, Configs.BasePath);
+      Assert.AreEqual("OldValue", Configs.GetValue<string>("BasePathDiff"));
+      Configs.Dispose();
+
+      var newBasePath = Path.Combine(Directory.GetCurrentDirectory(), "Test");
+      var appsettingsPath = Path.Combine(Directory.GetCurrentDirectory(), "appsettings.json");
+      var appSettings = File.ReadAllText(appsettingsPath).Replace("OldValue", "NewValue");
+      Directory.CreateDirectory(newBasePath);
+      File.WriteAllText(appsettingsPath.Replace("appsettings", "Test/appsettings"), appSettings);
+      Configs.Init(basePath : newBasePath);
+      Assert.AreEqual(newBasePath, Configs.BasePath);
+      Assert.AreEqual("Hello World!", Configs.GetValue<string>("GlobalKey"));
+      Assert.AreEqual("NewValue", Configs.GetValue<string>("BasePathDiff"));
+      Directory.Delete(newBasePath, true);
+      Configs.Dispose();
+    }
+
+    [TestMethod]
+    public void TestInMemoryData() {
+      Configs.Init();
+      Assert.IsNull(Configs.GetValue<string>("InMemoryKey"));
+      Configs.Dispose();
+
+      var memoryConfigs = new Dictionary<string, string>();
+      memoryConfigs.Add("InMemoryKey", "MemoryValue");
+      Configs.Init(inMemoryConfigs : memoryConfigs);
+      Assert.AreEqual("MemoryValue", Configs.GetValue<string>("InMemoryKey"));
+      Configs.Dispose();
+    }
+
+    [TestMethod]
+    public void TestConfigReload() {
+      Configs.Init();
+      var oldValue = Configs.GetValue<string>("Reloaded");
+      var appsettingsPath = Path.Combine(Directory.GetCurrentDirectory(), "appsettings.json");
+      var appsettings = File.ReadAllText(appsettingsPath);
+      var pattern = "\"Reloaded\":\\s\"(.+?)\"";
+      var replaceValue = Regex.Match(appsettings, pattern).Groups[1].Value;
+      var guid = Guid.NewGuid().ToString("N");
+      appsettings = appsettings.Replace(replaceValue, guid);
+      File.WriteAllText(appsettingsPath, appsettings);
+      Thread.Sleep(1000); // force wait to system detect changes
+      var newValue = Configs.GetValue<string>("Reloaded");
+      Assert.AreNotEqual(oldValue, newValue);
+      Assert.AreEqual(newValue, guid);
+      Configs.Dispose();
+    }
+
+    [TestMethod]
+    public void TestOverride() {
+      Configs.Init();
+      Assert.AreEqual("GlobalValue", Configs.GetValue<string>("Overridden"));
+      Configs.Dispose();
+    }
+
+    [TestMethod]
+    public void TestGetValue() {
+      Assert.AreEqual(10, Configs.GetValue<int>("IntValue"));
+      Assert.IsTrue(Configs.GetValue<bool>("BoolValue"));
+      Assert.AreEqual("Test", Configs.GetValue<string>("StringValue"));
+      Assert.AreEqual(new DateTime(2017, 01, 02, 12, 13, 14),
+        Configs.GetValue<DateTime>("DateTimeValue"));
+      Configs.Dispose();
+    }
+
+    [TestMethod]
+    public void TestInvalidGetValue() {
+      Assert.IsNull(Configs.GetValue<string>("InvalidKey"));
+      Assert.IsFalse(Configs.GetValue<bool>("InvalidKey"));
+      Configs.Dispose();
+    }
+
+    [TestMethod]
+    public void TestGetValueOrDefault() {
+      var defaultDate = new DateTime(2017, 01, 01);
+
+      Assert.AreEqual(10, Configs.GetValueOrDefault<int>("IntValue", 0));
+      Assert.IsTrue(Configs.GetValueOrDefault<bool>("BoolValue", false));
+      Assert.AreEqual("Test", Configs.GetValueOrDefault<string>("StringValue", "default"));
+      Assert.AreEqual(new DateTime(2017, 01, 02, 12, 13, 14),
+        Configs.GetValueOrDefault<DateTime>("DateTimeValue", defaultDate));
+
+      Assert.AreEqual(5, Configs.GetValueOrDefault<int>("InvalidKey", 5));
+      Assert.IsTrue(Configs.GetValueOrDefault<bool>("InvalidKey", true));
+      Assert.AreEqual("default", Configs.GetValueOrDefault<string>("InvalidKey", "default"));
+      Assert.AreEqual(defaultDate, Configs.GetValueOrDefault<DateTime>("InvalidKey", defaultDate));
+      Configs.Dispose();
+    }
+
+    [TestMethod]
+    public void TestGetArray() {
+      var intArray = new [] { 1, 2, 3 };
+      var strArray = new [] { "Value1", "Value2" };
+      Assert.IsTrue(intArray.SequenceEqual(Configs.GetArray<int>("IntArray")));
+      Assert.IsTrue(strArray.SequenceEqual(Configs.GetArray<string>("StringArray")));
+      Configs.Dispose();
+    }
+
+    [TestMethod]
+    public void TestGetConnectionString() {
+      var _default = @"Server=(localdb)\mssqllocaldb;Database=EFProviders.InMemory;Trusted_Connection=True;";
+      var mysql = "Server=localhost;Port=3306;Database=mydatabase-dev;Uid=root;Pwd=root;";
+      Assert.AreEqual(_default, Configs.GetConnectionString());
+      Assert.AreEqual(mysql, Configs.GetConnectionString("MySql"));
+      Configs.Dispose();
+    }
+
+    [TestMethod]
+    public void TestGetDirectory() {
+      var dirpath = Path.Combine(Directory.GetCurrentDirectory(), "TestDir");
+      if (Directory.Exists(dirpath))
+        Directory.Delete(dirpath, recursive : true);
+      var dir = Configs.GetDirectory("DirName");
+      Assert.IsNull(Configs.GetDirectory(""));
+      Assert.AreEqual(new DirectoryInfo(dirpath).FullName, dir.FullName);
+      Assert.IsTrue(Directory.Exists(dirpath));
+      var relativeBasePath = Path.Combine(dirpath, "TestDir");
+      var relativeDirPath = Path.Combine(relativeBasePath, "TestDir");
+      var relativeDir = Configs.GetDirectory("DirName", basePath : relativeBasePath);
+      Assert.AreEqual(new DirectoryInfo(relativeDirPath).FullName, relativeDir.FullName);
+      Assert.IsTrue(Directory.Exists(relativeDirPath));
+      Configs.Dispose();
+    } 
+
+    internal class MyOptions {
+      public int IntValue { get; set; }
+      public bool BoolValue { get; set; }
+      public string StringValue { get; set; }
+      public DateTime DateTimeValue { get; set; }
+      public IEnumerable<string> StringArray { get; set; }
+      public int[] IntArray { get; set; }
+      public MySubOptions MySubOptions { get; set; }
+    }
+
+    internal class MySubOptions {
+      public string Option1 { get; set; }
+      public string Option2 { get; set; }
+      public string Option3 { get; set; }
+    }
+
+    [TestMethod]
+    public void Test() {
+      var intArray = new [] { 1, 2, 3 };
+      var strArray = new [] { "Value1", "Value2" };
+      var model = Configs.BindTo<MyOptions>("MyOptions");
+      Assert.AreEqual(model.IntValue, 10);
+      Assert.IsTrue(model.BoolValue);
+      Assert.AreEqual(model.StringValue, "Test");
+      Assert.AreEqual(model.DateTimeValue, new DateTime(2017, 01, 02, 12, 13, 14));
+      Assert.IsNotNull(model.StringArray);
+      Assert.AreEqual(model.StringArray.Count(), 2);
+      Assert.IsTrue(strArray.SequenceEqual(model.StringArray));
+      Assert.IsNotNull(model.IntArray);
+      Assert.AreEqual(model.IntArray.Count(), 3);
+      Assert.IsTrue(intArray.SequenceEqual(model.IntArray));
+      Assert.IsNotNull(model.MySubOptions);
+      Assert.AreEqual(model.MySubOptions.Option1, "Lorem");
+      Assert.AreEqual(model.MySubOptions.Option2, "Ipsum");
+      Assert.IsNull(model.MySubOptions.Option3);
+    }
+  }
+}
