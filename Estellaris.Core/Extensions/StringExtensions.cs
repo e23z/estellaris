8e743714dcda26e0834536ca797be0c038e0f4af
Edio de Oliveira Zalewski <edio.o.zalewski@gmail.com>
2017-08-08 13:20:36 -0300

    add regex substitution to extensions

--- Estellaris.Core/Extensions/StringExtensions.cs
+++ Estellaris.Core/Extensions/StringExtensions.cs
@@ -1,21 +1,32 @@
 using System;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace Estellaris.Core.Extensions {
   public static class StringExtensions {
-    public static byte[ ] HexToBytes(this string hex) {
+    static readonly RegexOptions _options = RegexOptions.IgnoreCase | RegexOptions.Multiline | RegexOptions.Singleline;
+
+    public static byte[] HexToBytes(this string hex) {
       var bytes = new byte[hex.Length / 2];
       for (var i = 0; i < hex.Length; i += 2)
         bytes[i / 2] = Convert.ToByte(hex.Substring(i, 2), 16);
       return bytes;
     }
 
-    public static byte[ ] ToBytes(this string str) {
+    public static byte[] ToBytes(this string str) {
       return Encoding.UTF8.GetBytes(str);
     }
 
-    public static byte[ ] FromBase64(this string str) {
+    public static byte[] FromBase64(this string str) {
       return Convert.FromBase64String(str);
+    }
+
+    public static string Substitute(this string input, string pattern, string replacement) {
+      return input.Substitute(pattern, replacement, _options);
+    }
+
+    public static string Substitute(this string input, string pattern, string replacement, RegexOptions options) {
+      return Regex.Replace(input, pattern, replacement, options);
     }
   }
 }
--- Estellaris.Tests/Extensions/StringExtensionsTests.cs
+++ Estellaris.Tests/Extensions/StringExtensionsTests.cs
@@ -1,4 +1,5 @@
 using System.Linq;
+using System.Text.RegularExpressions;
 using Estellaris.Core.Extensions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -24,6 +25,15 @@ namespace Estellaris.Tests {
       var comparisonBytes = new byte[] { 116, 101, 115, 116 };
       var bytesFromStr = "dGVzdA==".FromBase64();
       Assert.IsTrue(bytesFromStr.SequenceEqual(comparisonBytes));
+    }
+    
+    [TestMethod]
+    public void TestSubstitute() {
+      var str = "My replaceable text";
+      var replaced = str.Substitute(@"\bREPlaceable\b", "beautiful");
+      var notReplaced = str.Substitute(@"\bREPlaceable\b", "beautiful", RegexOptions.None);
+      Assert.AreEqual(replaced, "My beautiful text");
+      Assert.AreEqual(notReplaced, str);
     }
   }
 }
