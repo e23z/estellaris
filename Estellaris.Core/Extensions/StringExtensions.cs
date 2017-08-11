8c0e9e489fb0ab9a78bf30f8b9520832851ea996
Edio de Oliveira Zalewski <edio.o.zalewski@gmail.com>
2017-08-11 17:08:03 -0300

    add regex and datetime extensions for strings

--- Estellaris.Core/Extensions/StringExtensions.cs
+++ Estellaris.Core/Extensions/StringExtensions.cs
@@ -1,4 +1,7 @@
 using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 
@@ -26,7 +29,63 @@ namespace Estellaris.Core.Extensions {
     }
 
     public static string Substitute(this string input, string pattern, string replacement, RegexOptions options) {
+      if (string.IsNullOrWhiteSpace(pattern) || string.IsNullOrWhiteSpace(replacement))
+        return input;
       return Regex.Replace(input, pattern, replacement, options);
+    }
+
+    public static Match Match(this string input, string pattern) {
+      return input.Match(pattern, _options);
+    }
+
+    public static Match Match(this string input, string pattern, RegexOptions options) {
+      if (string.IsNullOrEmpty(input) || string.IsNullOrWhiteSpace(pattern))
+        return null;
+      return Regex.Match(input, pattern, options);
+    }
+
+    public static IEnumerable<Match> Matches(this string input, string pattern) {
+      return input.Matches(pattern, _options);
+    }
+
+    public static IEnumerable<Match> Matches(this string input, string pattern, RegexOptions options) {
+      if (string.IsNullOrEmpty(input) || string.IsNullOrWhiteSpace(pattern))
+        return Enumerable.Empty<Match>();
+      return Regex.Matches(input, pattern, options).OfType<Match>();
+    }
+
+    public static bool IsMatch(this string input, string pattern) {
+      return input.IsMatch(pattern, _options);
+    }
+
+    public static bool IsMatch(this string input, string pattern, RegexOptions options) {
+      if (string.IsNullOrEmpty(input) || string.IsNullOrWhiteSpace(pattern))
+        return false;
+      return Regex.IsMatch(input, pattern, options);
+    }
+
+    public static string Truncate(this string input, int maxLength, string ending = "...") {
+      if (string.IsNullOrWhiteSpace(input) || input.Length <= maxLength)
+        return input;
+      return input.Substring(0, maxLength) + ending;
+    }
+
+    public static DateTime ToDateTime(this string str, CultureInfo culture = null) {
+      return str.ToDateTime("", culture);
+    }
+
+    public static DateTime ToDateTime(this string str, string format, CultureInfo culture = null) {
+      if (culture == null)
+        culture = CultureInfo.CurrentCulture;
+
+      var date = new DateTime();
+
+      if (string.IsNullOrWhiteSpace(format))
+        DateTime.TryParse(str, culture, DateTimeStyles.None, out date);
+      else
+        DateTime.TryParseExact(str, format, culture, DateTimeStyles.None, out date);
+
+      return date;
     }
   }
 }
--- Estellaris.Tests/Extensions/StringExtensionsTests.cs
+++ Estellaris.Tests/Extensions/StringExtensionsTests.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Globalization;
 using System.Linq;
 using System.Text.RegularExpressions;
 using Estellaris.Core.Extensions;
@@ -34,6 +36,62 @@ namespace Estellaris.Tests {
       var notReplaced = str.Substitute(@"\bREPlaceable\b", "beautiful", RegexOptions.None);
       Assert.AreEqual(replaced, "My beautiful text");
       Assert.AreEqual(notReplaced, str);
+    }
+
+    [TestMethod]
+    public void TestTruncate() {
+      var untruncated = "My huge text line to be truncated";
+      var truncated = untruncated.Truncate(10);
+      var truncatedEnding = untruncated.Truncate(10, "[...]");
+      Assert.IsFalse(string.IsNullOrWhiteSpace(truncated));
+      Assert.IsFalse(string.IsNullOrWhiteSpace(truncatedEnding));
+      Assert.AreEqual("My huge te...", truncated);
+      Assert.AreEqual("My huge te[...]", truncatedEnding);
+    }
+    
+    [TestMethod]
+    public void TestMatch() {
+      var text = "It's a beautiful world full of Hello World's tests.";
+      var match = text.Match(@"(\bbeautiful\b\s[a-z]+)");
+      var nullMatch = text.Match("");
+      Assert.IsNull(nullMatch);
+      Assert.IsNotNull(match);
+      Assert.AreEqual("beautiful world", match.Groups[1].Value);
+    }
+    
+    [TestMethod]
+    public void TestMatches() {
+      var text = "It's a beautiful world full of Hello World's tests.";
+      var matches = text.Matches(@"world");
+      var emptyMatches = text.Matches("");
+      Assert.IsTrue(emptyMatches.IsEmpty());
+      Assert.IsFalse(matches.IsEmpty());
+      Assert.AreEqual(2, matches.Count());
+      Assert.AreEqual("world", matches.First().Value);
+      Assert.AreEqual("World", matches.Last().Value);
+    }
+    
+    [TestMethod]
+    public void TestIsMatch() {
+      var text = "It's a beautiful world full of Hello World's tests.";
+      Assert.IsFalse(text.IsMatch(""));
+      Assert.IsTrue(text.IsMatch(@"tests\.$"));
+    }
+    
+    [TestMethod]
+    public void TestToDateTime() {
+      var culture = new CultureInfo("pt-BR");
+      var date = "2017-02-01".ToDateTime();
+      var localizedDate = "31/03/2017".ToDateTime(culture);
+      var customFormat = "July, 02 2017".ToDateTime("MMMM, dd yyyy");
+      var localizedCustomFormat = "20 de janeiro de 2017".ToDateTime("dd 'de' MMMM 'de' yyyy", culture);
+      var invalidDate = "31/31/1254".ToDateTime();
+
+      Assert.AreEqual(new DateTime(2017, 02, 01), date);
+      Assert.AreEqual(new DateTime(2017, 03, 31), localizedDate);
+      Assert.AreEqual(new DateTime(2017, 07, 02), customFormat);
+      Assert.AreEqual(new DateTime(2017, 01, 20), localizedCustomFormat);
+      Assert.AreEqual(new DateTime(), invalidDate);
     }
   }
 }
